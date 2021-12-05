3378c767a85d144143c3a08230fb6fd373a08d80
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-12-05 16:10:26 +0900

    add

--- 22.generate-parentheses.py
+++ 22.generate-parentheses.py
@@ -5,17 +5,40 @@
 #
 
 # @lc code=start
+""" solution by dfs
 class Solution:
+    def dfs(self, res: List[str], lis: str, cnt: int, n: int):
+        if len(lis) == n*2 and cnt == n:
+            res.append(lis)
+            return True
+        if cnt < n:
+            self.dfs(res,lis+'(', cnt+1, n)
+        if cnt > len(lis)-cnt:
+            self.dfs(res,lis+')', cnt, n)
+
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        for bit in range(1<<(n*2)):
-            s = ''
-            for i in range(n*2):
-                if bit & (1<<i):
-                    s+=')'
-                else:
-                    s+='('
-            res.append(s)
+        self.dfs(res,'(',1, n)
         return res
+"""
+
+""" solution by dfs """
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        from collections import deque
+        q = deque()
+        q.append(('(', 1, 0))
+        res = []
+        while q:
+            s, l, r = q.pop()
+            if l+r==n*2:
+                res.append(s)
+                continue
+            if l < n:
+                q.append((s+'(', l+1, r))
+            if l>r:
+                q.append((s+')', l, r+1))
+        return res
+
 # @lc code=end
 
--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -5,6 +5,7 @@
 #
 
 # @lc code=start
+""" solution by dfs
 class Solution:
     def dfs(self, res: List[List[int]],now: List[int], lis: List[int], target: int, before: int) -> bool:
         if target == 0:
@@ -19,4 +20,24 @@ class Solution:
         res = []
         self.dfs(res,[], sorted(candidates), target, 0)
         return res
+"""
+
+"""solution by bfs"""
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        from collections import deque
+        q = deque()
+        q.append((target, [], 0))
+        candidates.sort()
+        res = []
+        while q:
+            val, lis, index = q.popleft()
+            for i in range(index,len(candidates)):
+                if val - candidates[i] == 0:
+                    res.append(lis+[candidates[i]])
+                if val - candidates[i] < 0:
+                    continue
+                q.append((val-candidates[i], lis+[candidates[i]], i))
+        return res
+
 # @lc code=end
